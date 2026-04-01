b72a96a563757e9f76b569c01bf73ecd7e1cc632
phsmyki222 <rollexbmw@gmail.com>
2026-04-01 19:47:42 +0100

    Update phrinusomyis_update.py

--- phrinusomyis_update.py
+++ phrinusomyis_update.py
@@ -2,18 +2,21 @@ updates = [
     {
         "title": "PHRINUSOMYIS Announces Tatiana Manaois",
         "link": "https://www.phrinusomyis.com/news/tatiana-manaois",
-        "description": "PHRINUSOMYIS proudly announces the international debut of Tatiana Manaois, a world-class singer and performer, at our West Africa stage. International Debut: June 26, 2026.",
-        "image": "images/tatiana-manaois.jpg",
+        "description": "PHRINUSOMYIS proudly announces the international debut at our West Africa stage. International Debut: June 26, 2026.",
+        "image": "https://www.phrinusomyis.com/images/tatiana-manaois.jpg",
         "pubDate": "Wed, 01 Apr 2026 12:00:00 +0000",
         "guid": "tatiana-manaois-2026"
     },
     {
         "title": "New Artist Signed",
-        "content": "PHRINUSOMYIS welcomes our newest talent to the roster.",
-        "date": "2026-04-01"
+        "link": "https://www.phrinusomyis.com/news/new-artist",
+        "description": "PHRINUSOMYIS welcomes our newest talent to the roster.",
+        "image": "https://www.phrinusomyis.com/images/new-artist.jpg",
+        "pubDate": "Wed, 01 Apr 2026 15:00:00 +0000",
+        "guid": "new-artist-2026"
     }
 ]
 
-# Example: Print all titles
+# Print all titles
 for update in updates:
     print(update["title"])
