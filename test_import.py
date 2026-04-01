a445e51ef03117e63c3cb323a0b6be010bae43c1
phsmyki222 <rollexbmw@gmail.com>
2026-04-01 20:04:15 +0100

    Create test_import.py

---
+++ test_import.py
@@ -0,0 +1,5 @@
+import phrinusomyis_update
+import phrinusomyis_intelligence
+
+print("Updates loaded:", len(phrinusomyis_update.updates))
+print("SEO keywords loaded:", len(phrinusomyis_intelligence.seo_keywords))
