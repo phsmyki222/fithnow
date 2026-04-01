3d1d6a0634072344326c83f6272e703783dc6a31
phsmyki222 <rollexbmw@gmail.com>
2026-04-01 23:58:49 +0100

    Update phrinusomyis_update.py

--- phrinusomyis_update.py
+++ phrinusomyis_update.py
@@ -1,10 +1,6 @@
-updates = [
-    {
-        "title": "PHRINUSOMYIS Announces Tatiana Manaois",
-        "link": "https://www.phrinusomyis.com/news/tatiana-manaois",
-        "description": "International Debut June 26, 2026.",
-        "image": "https://www.phrinusomyis.com/images/tatiana-manaois.jpg",
-        "pubDate": "Wed, 01 Apr 2026 12:00:00 +0000",
-        "guid": "tatiana-manaois-2026"
-    }
-]
+import json
+
+updates_file = "phrinusomyis_update.py"
+
+with open(updates_file, "r", encoding="utf-8") as f:
+    updates = json.load(f)   # instead of py.load(f)
