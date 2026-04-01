81d75f4c5ede21a9ffd76e7ce187cdb4b14c6f15
phsmyki222 <rollexbmw@gmail.com>
2026-04-01 19:54:08 +0100

    Update phrinusomyis_update.py

--- phrinusomyis_update.py
+++ phrinusomyis_update.py
@@ -2,16 +2,16 @@ updates = [
     {
         "title": "PHRINUSOMYIS Announces Tatiana Manaois",
         "link": "https://www.phrinusomyis.com/news/tatiana-manaois",
-        "description": "International debut June 26, 2026.",
-        "image": "images/tatiana-manaois.jpg",
+        "description": "PHRINUSOMYIS proudly announces the international debut. International Debut: June 26, 2026.",
+        "image": "https://www.phrinusomyis.com/images/tatiana-manaois.jpg",
         "pubDate": "Wed, 01 Apr 2026 12:00:00 +0000",
         "guid": "tatiana-manaois-2026"
     },
     {
         "title": "New Artist Signed",
         "link": "https://www.phrinusomyis.com/news/new-artist",
-        "description": "PHRINUSOMYIS welcomes our newest talent.",
-        "image": "images/new-artist.jpg",
+        "description": "PHRINUSOMYIS welcomes our newest talent to the roster.",
+        "image": "https://www.phrinusomyis.com/images/new-artist.jpg",
         "pubDate": "Wed, 01 Apr 2026 15:00:00 +0000",
         "guid": "new-artist-2026"
     }
