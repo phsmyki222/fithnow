d269a9d2c85bcce81e2e083f0aa1b6a3e8b0985f
phsmyki222 <rollexbmw@gmail.com>
2026-04-01 15:12:07 +0100

    Create phrinusomyis_update.py

---
+++ phrinusomyis_update.py
@@ -0,0 +1,19 @@
+updates = [
+    {
+        "title": "PHRINUSOMYIS Announces Tatiana Manaois",
+        "link": "https://www.phrinusomyis.com/news/tatiana-manaois",
+        "description": "PHRINUSOMYIS proudly announces the international debut of Tatiana Manaois, a world-class singer and performer, at our West Africa stage. International Debut: June 26, 2026.",
+        "image": "images/tatiana-manaois.jpg",
+        "pubDate": "Wed, 01 Apr 2026 12:00:00 +0000",
+        "guid": "tatiana-manaois-2026"
+    },
+    {
+        "title": "New Artist Signed",
+        "content": "PHRINUSOMYIS welcomes our newest talent to the roster.",
+        "date": "2026-04-01"
+    }
+]
+
+# Example: Print all titles
+for update in updates:
+    print(update["title"])
